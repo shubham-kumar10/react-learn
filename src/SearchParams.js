97eac7a15d91dd68fc96c521b4e069ac4abca501
shubham.snu@gmail.com <shubham.snu@gmail.com>
2021-01-16 18:59:29 +0530

    :sparkles: useState examples

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 function SearchParams() {
   const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('dog');
+  const [breed, setBreed] = useState('breed');
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -16,6 +20,39 @@ function SearchParams() {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={!breeds.length}
+          >
+            <option>All</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
+        </label>
         <button>Submit</button>
       </form>
     </div>
@@ -25,3 +62,4 @@ function SearchParams() {
 export default SearchParams;
 
 // Hooks can never be inside for loops or if statements
+// key tells React that only some reorder has been done, but should be unique

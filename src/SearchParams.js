d7f4493fec67bdc12fc1ae8f4b3e6cece1047198
shubham.snu@gmail.com <shubham.snu@gmail.com>
2021-01-20 00:25:01 +0530

    feat: use effects added

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useEffect, useState } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 function SearchParams() {
@@ -9,7 +9,20 @@ function SearchParams() {
 
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+
+  // Use Effect runs after the render happens.
+  // It is not synchronous so doesnt run just after the render but soon after the render
+  // 2nd parameter is a list of dependancies, the change in dependencies trigger useEffect to run.
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed('');
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedString = breeds.map(({ name }) => name);
+      setBreeds(breedString);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
